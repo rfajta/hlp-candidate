ca878df5a903de107ac050f067682f06469417ac
Robert Fajta <robert@digitalasset.com>
2016-03-24 17:54:47 -0400

    rearrange the test a bit and adding comments to it

--- server/regtest/src/test/java/org/hyperledger/account/AccountOperationsViaGrpcTest.java
+++ server/regtest/src/test/java/org/hyperledger/account/AccountOperationsViaGrpcTest.java
@@ -44,38 +44,50 @@ public class AccountOperationsViaGrpcTest {
 
     @Test
     public void sendToAddressTest() throws Exception {
-        BCSAPI api = regtestRule.getBCSAPI();
-        int startingChainHeight = api.getChainHeight();
-
+        // setting up sender's account and receiver's read only accounts
         PrivateKey priv = PrivateKey.createNew();
         KeyListChain senderKeyChain = new KeyListChain(priv);
         BaseAccount senderAccount = new BaseAccount(senderKeyChain);
-        api.registerTransactionListener(senderAccount);
-
-        Transaction tx = Transaction.create()
-                .inputs(TransactionInput.create().source(TID.INVALID, -1).build())
-                .outputs(TransactionOutput.create().payTo(priv.getAddress()).value(100000000).build())
-                .build();
-
-        ActionWaiter.execute(() -> api.sendTransaction(tx), expected(senderAccount, 1));
-
-        int newChainHeight = api.getChainHeight();
-        assertEquals("No block was created", startingChainHeight + 1, newChainHeight);
-        assertEquals("Incorrect balance", 100000000, senderAccount.getCoins().getTotalSatoshis());
 
         Address receiverAddress = PrivateKey.createNew().getAddress();
         ReadOnlyAccount receiverAccount = new BaseReadOnlyAccount(new AddressListChain(receiverAddress));
+
+        // getting the interface towards obs
+        BCSAPI api = regtestRule.getBCSAPI();
+
+        // starting chain height
+        int startingChainHeight = api.getChainHeight();
+
+        // accounts start listening to the changes in the accounts
+        api.registerTransactionListener(senderAccount);
         api.registerTransactionListener(receiverAccount);
 
-        TransactionFactory factory = new BaseTransactionFactory(senderAccount);
+        // transaction to fund the sender's account with coinbase
+        Transaction fundingTx = Transaction.create()
+                .inputs(TransactionInput.create().source(TID.INVALID, -1).build())
+                .outputs(TransactionOutput.create().payTo(priv.getAddress()).value(100000).build())
+                .build();
 
+        // execute the funding transaction
+        ActionWaiter.execute(() -> api.sendTransaction(fundingTx), expected(senderAccount, 1));
+
+        // expect that a new block was created with the funding tx
+        int newChainHeight = api.getChainHeight();
+        assertEquals("No block was created", startingChainHeight + 1, newChainHeight);
+        // expect that sender's account now have the intended funding
+        assertEquals("Incorrect balance", 100000, senderAccount.getCoins().getTotalSatoshis());
+
+        // transaction to pay some of the funding forward
+        TransactionFactory factory = new BaseTransactionFactory(senderAccount);
         Transaction nextTx = factory.propose(receiverAddress, 30000).sign(senderKeyChain);
+
+        // execute the transaction
         ActionWaiter.execute(() -> api.sendTransaction(nextTx), expectedOneOnEach(senderAccount, receiverAccount));
 
+        // expect the receiver got the transfer
         assertEquals("Incorrect balance", 30000, receiverAccount.getCoins().getTotalSatoshis());
-        assertEquals("Incorrect confirmed", 30000, receiverAccount.getConfirmedCoins().getTotalSatoshis());
-        // 5000 less because the transaction has an output with the tx fee
-        assertEquals("Incorrect balance", 99965000, senderAccount.getCoins().getTotalSatoshis());
-        assertEquals("Incorrect confirmed", 99965000, senderAccount.getConfirmedCoins().getTotalSatoshis());
+        // expect the sender's balance was decreased
+        // it is 5000 less because the transaction costed fee of 5000
+        assertEquals("Incorrect balance", 65000, senderAccount.getCoins().getTotalSatoshis());
     }
 }
